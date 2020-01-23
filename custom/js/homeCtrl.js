b42945f2c5c164284767fa762d6f3336c5141012
Matias <matiasmicheletto@gmail.com>
2020-01-23 20:24:04 -0300

    Reportar eventos en otros lugares del mapa

--- custom/js/homeCtrl.js
+++ custom/js/homeCtrl.js
@@ -10,14 +10,22 @@ var homeCtrl = function () { // Controller vista home
 
 
     /////// Inicializar mapa //////////
-    var current_location = null; // Ubicacion actual del usuario
+    var current_location = null; // Ubicacion actual del usuario (Obj: {marker, accuracy})
+    var tap_location = null; // Posicion donde clickea en el mapa (para reportar evento) (Obj: {lat, long})
     var marker_list = []; // Lista de marcadores (wus, eventos)
+    
     var map = L.map('map').fitWorld();
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         maxZoom: 18,
         id: 'mapbox/streets-v11',
         accessToken: 'pk.eyJ1IjoibWF0aWFzbWljaGVsZXR0byIsImEiOiJjazVsa2ZtamowZHJnM2ttaXFmZGo1MDhtIn0.8iBO-J1wj34LIqq-e4Me5w'
     }).addTo(map);
+
+    // Callback de click o tap: mostrar menu para reportarr evento aqui
+    map.on('click', function(e){
+        tap_location = e.latlng;
+        event_dialog.open();
+    });
 
     // Callback de GPS
     map.on('locationfound', function (e) {
@@ -94,10 +102,16 @@ var homeCtrl = function () { // Controller vista home
             text: '<img src="'+ev.icon+'" style="max-width:50px;vertical-align: middle;"/> <span style="margin-left:20px;font-size:1.1em;"> '+ev.text+'</span>',
             onClick: function () { // Callback de click de cada boton
                     // Agregar marcador a la lista que tiene el mapa
-                    console.log(type);
-                    console.log(event_types[type].text);
-                    marker_list.push(L.marker(current_location.marker.getLatLng()));
-                    marker_list[marker_list.length-1].addTo(map).bindPopup(event_types[type].text+" near this location.").openPopup();
+                    //console.log(event_types[type].text);
+                    if(tap_location){
+                        marker_list.push(L.marker(tap_location)); // TODO: cuando se conecte a un WU, reportar esta lista
+                        marker_list[marker_list.length-1].addTo(map).bindPopup(event_types[type].text+" near this location.").openPopup();
+                    }else{
+                        marker_list.push(L.marker(current_location.marker.getLatLng())); // TODO: cuando se conecte a un WU, reportar esta lista
+                        marker_list[marker_list.length-1].addTo(map).bindPopup(event_types[type].text+" near this location.").openPopup();
+                    }
+
+                    // TODO: reportar evento al WU actual
                 }
         });
     });
@@ -107,6 +121,7 @@ var homeCtrl = function () { // Controller vista home
         title: 'Report event',
         text: 'Select the type of event from the list to report for your current location',
         buttons: buttons,
+        closeByBackdropClick: true,
         verticalButtons: true,
     });
 
@@ -117,6 +132,7 @@ var homeCtrl = function () { // Controller vista home
             button.className = "button button-raised button-fill color-white text-color-black";
             button.innerHTML = "<i class='material-icons'>room</i>";
             button.onclick = function () { // Evento de clickeo del boton
+                tap_location = null; // Borrar posicion de click para que se use la posicion actual
                 event_dialog.open(); // Abrir dialogo
             };
             return button;
