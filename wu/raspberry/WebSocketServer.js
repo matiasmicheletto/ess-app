38de9b2dc733ae6905b815015c2eb1fbab1f52c4
Matias <matiasmicheletto@gmail.com>
2020-03-30 14:49:26 -0300

    MQTT para Websocketserver

--- wu/raspberry/WebSocketServer.js
+++ wu/raspberry/WebSocketServer.js
@@ -7,13 +7,21 @@
 //    $ node ./WebScoketServer.js
 //
 
-const WebSocketServer = require('ws').Server; 
-const Fs = require('fs');
+const WebSocketServer = require('ws').Server; // WSS
+const MQTT = require('mqtt'); // MQTT
+const Fs = require('fs'); // File Storage
+
+// Topics mqtt
+const inTopic = 'wu1/messajeIn';
+const outTopic = 'wu1/messajeOut';
+
+
+/////// WEB SOCKETS ////////
 
 //var wss = new WebSocketServer({port: 8081}); // Servidor WebSocket en puerto 8081 para debug
 var wss = new WebSocketServer({port: 443}); // Servidor WebSocket en puerto 443
 
-var clients = []; // Lista de clientes conectados
+var clients = []; // Lista de clientes conectados al web socket server
 
 wss.on('connection', function (cl) { // Callback de conexion con nuevo cliente
 
@@ -39,35 +47,60 @@ wss.on('connection', function (cl) { // Callback de conexion con nuevo cliente
     });
 
     cl.on('close', function () { // Callback cierre de conexion
-        clients.splice(cl.index,1); // Quitar del arreglo
+        clients.splice(cl.index, 1); // Quitar del arreglo
         console.log("Cliente desconectado.");
         //console.log(clients);
     });
 });
 
+console.log("Server WU esperando conexiones...");
 
-var loadDatabase = function(){ // Leer base de datos mas reciente desde archivo
+
+
+///// LECTURA / ESCRITURA DB ///////
+
+var loadDatabase = function () { // Leer base de datos mas reciente desde archivo
 
     var database = null;
-    try{
+    try {
         database = JSON.parse(Fs.readFileSync('database.json'));
-    } catch(err){
+    } catch (err) {
         console.log(err);
     }
 
-    if(database)
+    if (database)
         return database;
 };
 
-var writeDatabase = function(db){ // Guardar nueva database en nuevo archivo
+var writeDatabase = function (db) { // Guardar nueva database en nuevo archivo
     Fs.writeFileSync('database.json', JSON.stringify(db));
 };
 
-var mergeDatabases = function(local_db, remote_db){ // Combinar informacion
+var mergeDatabases = function (local_db, remote_db) { // Combinar informacion
 
     // TODO: Algoritmo de combinacion de informacion
 
     return local_db;
 };
 
-console.log("Server WU esperando conexiones...");
+
+
+
+//////// MQTT //////////////
+
+var mqttClient = MQTT.connect('mqtt://localhost');
+
+mqttClient.on('connect', function () { // Callback conexion
+    mqttClient.subscribe(inTopic, function (err) {
+        if(err) console.log(err);
+    });
+});
+
+mqttClient.on('message', function (topic, message) { // Mensaje es tipo Buffer
+    console.log("["+topic+"] - Mensaje del broker: "+message.toString());
+    mqttClient.end(); // Esto va?
+});
+
+var mqttSend = function(payload){ // Enviar mensaje al broker
+    mqttClient.publish(outTopic, payload);
+};
